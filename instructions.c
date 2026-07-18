1765efc14cb38b58820c0ca2f1dc4d336ee6fb8f
varun007-code <millionaire3725@gmail.com>
2026-07-18 23:59:27 +0530

    it's second task

---
+++ instructions.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <capstone/capstone.h>
+
+int main(void)
+{
+    char str[] = "Hello World";
+
+    printf("String: %s\n", str);
+    printf("String Address: %p\n\n", (void *)str);
+
+    unsigned char *code = (unsigned char *)main;
+
+    csh handle;
+    cs_insn *insn;
+    size_t count;
+
+    if (cs_open(CS_ARCH_X86, CS_MODE_64, &handle) != CS_ERR_OK)
+        return -1;
+
+    count = cs_disasm(handle, code, 64, (uint64_t)code, 0, &insn);
+
+    if (count > 0) {
+        printf("Disassembly of main():\n\n");
+
+        for (size_t i = 0; i < count; i++) {
+            printf("0x%llx:\t%s\t%s\n",
+                   insn[i].address,
+                   insn[i].mnemonic,
+                   insn[i].op_str);
+        }
+
+        cs_free(insn, count);
+    }
+
+    cs_close(&handle);
+
+    return 0;
+}
