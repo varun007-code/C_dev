47d75dee42cbe8462e6b2274627ac305d76486e1
varun007-code <millionaire3725@gmail.com>
2026-07-27 07:08:10 +0530

    add define in forloop

--- Forloop.c
+++ Forloop.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
 
 /* print Fahrenheit-Celsius table */
 /*here in the for loop
@@ -12,7 +15,7 @@ int main() {
     printf("Fahrenheit to Celsius Conversion Table\n");
     printf("--------------------------------------\n");
     printf("Fahrenheit\tCelsius\n");
-    for(fahr = 300; fahr >=0; fahr=fahr-20)
+    for(fahr = LOWER; fahr <=UPPER; fahr+=STEP)
         printf("%3d\t%6.1f\n",fahr, (5.0/9.0)*(fahr-32));
     return 0;
 }
