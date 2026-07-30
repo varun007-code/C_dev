77ca5cace460e60690d5eca3491d1030d07fabc2
varun007-code <millionaire3725@gmail.com>
2026-07-30 22:22:55 +0530

    char count program

---
+++ charcount.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/*
+int main()
+{
+    long nc;
+    nc = 0;
+    while(getchar() != EOF)
+    {
+        ++nc;
+        printf("%ld\n",nc);
+    }
+} */
+
+int main()
+{
+    double nc;
+
+    for(nc=0; getchar() != EOF;++nc) 
+        printf("%.0f\n",nc);
+    
+
+}
